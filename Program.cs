dd555b361e49fe1b29aa0bb56b1dce92ae8a9ecf
Timothy Graupmann <tim.graupmann@razer.com>
2021-01-08 16:07:35 -0800

    Added input and init

--- Program.cs
+++ Program.cs
@@ -1,15 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ChromaSDK;
+using System;
+using System.Threading;
 
 namespace CSharp_ChromaGameLoopSample
 {
     class Program
     {
+        static void PrintLegend(SampleApp sampleApp)
+        {
+            Console.WriteLine(@"C# GAME LOOP CHROMA SAMPLE APP
+
+Press `ESC` to Quit.
+Press `A` for ammo/health.
+Press `F` for fire.
+Press `H` to toggle hotkeys.
+Press `L` for landscape.
+Press `R` for rainbow.
+Press `S` for spiral.");
+        }
+
         static void Main(string[] args)
         {
+            SampleApp sampleApp = new SampleApp();
+            sampleApp.Start();
+
+            if (sampleApp.GetInitResult() == RazerErrors.RZRESULT_SUCCESS)
+            {
+                while (true)
+                {
+                    Console.Clear();
+                    PrintLegend(sampleApp);
+                    ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                    if (keyInfo.Key == ConsoleKey.Escape)
+                    {
+                        sampleApp.OnApplicationQuit();
+                        break;
+                    }
+                    else if (keyInfo.Key == ConsoleKey.A)
+                    {
+                        //sampleApp.ExecuteItem();
+                    }
+                    else if (keyInfo.Key == ConsoleKey.F)
+                    {
+                        //sampleApp.ExecuteItem();
+                    }
+                    else if (keyInfo.Key == ConsoleKey.H)
+                    {
+                        //sampleApp.ExecuteItem();
+                    }
+                    else if (keyInfo.Key == ConsoleKey.L)
+                    {
+                        //sampleApp.ExecuteItem();
+                    }
+                    else if (keyInfo.Key == ConsoleKey.R)
+                    {
+                        //sampleApp.ExecuteItem();
+                    }
+                    else if (keyInfo.Key == ConsoleKey.S)
+                    {
+                        //sampleApp.ExecuteItem();
+                    }
+                    Thread.Sleep(1);
+                }
+
+                ChromaAnimationAPI.StopAll();
+                ChromaAnimationAPI.CloseAll();
+                sampleApp.OnApplicationQuit();
+
+            }
+
+            Console.WriteLine("{0}", "[EXIT]");
         }
     }
 }
---
+++ SampleApp.cs
@@ -0,0 +1,38 @@
+﻿using ChromaSDK;
+using System;
+
+namespace CSharp_ChromaGameLoopSample
+{
+    public class SampleApp
+    {
+        private int _mResult = 0;
+
+        public int GetInitResult()
+        {
+            return _mResult;
+        }
+
+        public void Start()
+        {
+            _mResult = ChromaAnimationAPI.Init();
+            switch (_mResult)
+            {
+                case RazerErrors.RZRESULT_DLL_NOT_FOUND:
+                    Console.Error.WriteLine("Chroma DLL is not found! {0}", RazerErrors.GetResultString(_mResult));
+                    break;
+                case RazerErrors.RZRESULT_DLL_INVALID_SIGNATURE:
+                    Console.Error.WriteLine("Chroma DLL has an invalid signature! {0}", RazerErrors.GetResultString(_mResult));
+                    break;
+                case RazerErrors.RZRESULT_SUCCESS:
+                    break;
+                default:
+                    Console.Error.WriteLine("Failed to initialize Chroma! {0}", RazerErrors.GetResultString(_mResult));
+                    break;
+            }
+        }
+        public void OnApplicationQuit()
+        {
+            ChromaAnimationAPI.Uninit();
+        }
+    }
+}
